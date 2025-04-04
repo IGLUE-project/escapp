06e4b052536f626ed6fc36716f138208a6160dd7
sonsoleslp <poynting.slp@gmail.com>
2025-04-04 13:30:37 +0300

    Fix some linting issues

--- migrations/20250327213951-AddFieldForUserEducationLevel.js
+++ migrations/20250327213951-AddFieldForUserEducationLevel.js
@@ -3,5 +3,5 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     "up": (queryInterface, Sequelize) => queryInterface.addColumn("users", "eduLevel", Sequelize.STRING),
-    "down": (queryInterface) => queryInterface.removeColumn("eduLevel", Sequelize.STRING)
+    "down": (queryInterface) => queryInterface.removeColumn("eduLevel")
 };
