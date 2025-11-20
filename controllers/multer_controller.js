914b8c796764254ebef93d4057157015eb4bf88b
agordillo <agordillos@gmail.com>
2025-11-20 12:21:19 +0100

    Add timestamp to file ids

--- controllers/multer_controller.js
+++ controllers/multer_controller.js
@@ -8,17 +8,17 @@ const storageWithExtension = (destination) => multer.diskStorage({
     "filename" (_, file, cb) {
         const originalNameParts = file.originalname.split(".");
         const extension = originalNameParts.length > 1 ? `.${originalNameParts.pop()}` : "";
-
-        cb(null, crypto.randomBytes(16).toString("hex") + extension);
+        const timestamp = Math.floor(Date.now() / 1000);
+        const random = crypto.randomBytes(8).toString("hex");
+        const fileId = timestamp+crypto.randomBytes(16).toString("hex");
+        cb(null, fileId + extension);
     }
 });
-
 
 const instructionStorage = storageWithExtension("./uploads/instructions/");
 const thumbnailStorage = storageWithExtension("./uploads/thumbnails/");
 const hybridStorage = storageWithExtension("./uploads/hybrid/");
 const uploadStorage = storageWithExtension("./uploads/");
-
 
 exports.instructions = multer({"storage": instructionStorage});
 exports.thumbnails = multer({"storage": thumbnailStorage});
